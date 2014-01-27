4c64966b7dd5e76f440fee4ea2e5552b2a2b9427
nilshell <nilshell@localhost>
2014-01-26 23:55:09 -0500

    attachment listing for arbitrary bug number

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import ( "fmt"; "./xmlrpc"; "log" )
+import ( "fmt"; "./xmlrpc"; "log"; "reflect" )
 
 func main() {
    client, _ := xmlrpc.NewClient("http://10.0.1.89/bugzilla/xmlrpc.cgi", nil)
@@ -15,13 +15,14 @@ func main() {
       log.Fatal(err)
       return
    }
-   fmt.Printf("User.login returned: %v\n", result)
+   //fmt.Printf("User.login returned: %v\n", result)
 
+   bug_number := "3"
    // get attachment data
    err = client.Call(
                         "Bug.attachments",
                         xmlrpc.Struct{
-                           "ids":[]string{"3"},
+                           "ids":[]string{bug_number},
                             "include_fields":[]string{
                                "file_name","id",
                                "last_change_time",
@@ -33,11 +34,17 @@ func main() {
       log.Fatal(err)
       return
    }
-   fmt.Printf("Bug.attachments returned: %v\n", result)
+   fmt.Printf("Bug.attachments returned: %v\n\n", result)
 
-   // version
-   /*
-   client.Call("Bugzilla.version",nil,&result)
-   fmt.Printf("Bugzilla.version returned: %v\n",result)
-   */
+   a := reflect.ValueOf(result["bugs"].(xmlrpc.Struct)[bug_number])
+   for i := 0; i < a.Len(); i++ {
+      attachment := a.Index(i).Interface().(xmlrpc.Struct)
+      //fmt.Printf("attachment [%d] : %v\n", i, attachment)
+
+      fmt.Printf("attachment [%d] ::\n", i)
+      fmt.Printf("\tfile_name : %v\n", attachment["file_name"])
+      fmt.Printf("\tid : %v\n", attachment["id"])
+      fmt.Printf("\tis_obsolete : %v\n", attachment["is_obsolete"])
+      fmt.Printf("\tlast_change_time : %v\n", attachment["last_change_time"])
+   }
 }
