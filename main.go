f8822b5cf83f92ab036bfc3fb08c12073b95b315
nilshell <nilshell@localhost>
2014-01-27 00:39:37 -0500

    added call to get base64 encoded attachment data

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import ( "fmt"; "./xmlrpc"; "log"; "reflect" )
 
+func check(err error) {
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
 func main() {
    client, _ := xmlrpc.NewClient("http://10.0.1.89/bugzilla/xmlrpc.cgi", nil)
    result := xmlrpc.Struct{}
@@ -11,40 +17,60 @@ func main() {
                       xmlrpc.Struct{"login":"test@localhost.localdomain",
                                     "password":"password"},
                       &result)
-   if err != nil {
-      log.Fatal(err)
-      return
-   }
+   check(err)
    //fmt.Printf("User.login returned: %v\n", result)
 
-   bug_number := "3"
    // get attachment data
+   bug_number := "3"
+
    err = client.Call(
                         "Bug.attachments",
                         xmlrpc.Struct{
                            "ids":[]string{bug_number},
                             "include_fields":[]string{
-                               "file_name","id",
+                               "file_name","id","description",
                                "last_change_time",
                                "is_obsolete"},
                         },
                         &result,
                     )
-   if err != nil {
-      log.Fatal(err)
-      return
-   }
-   fmt.Printf("Bug.attachments returned: %v\n\n", result)
+   check(err)
+   fmt.Printf("Bug.attachments() returned: %v\n\n", result)
 
    a := reflect.ValueOf(result["bugs"].(xmlrpc.Struct)[bug_number])
+
+   if a.Len() == 0 {
+      fmt.Printf("Bug id %s has no attachments.\n", bug_number)
+      return
+   }
+
+   id_list := []int64{}
    for i := 0; i < a.Len(); i++ {
       attachment := a.Index(i).Interface().(xmlrpc.Struct)
       //fmt.Printf("attachment [%d] : %v\n", i, attachment)
 
+      id_list = append(id_list, attachment["id"].(int64))
       fmt.Printf("attachment [%d] ::\n", i)
       fmt.Printf("\tfile_name : %v\n", attachment["file_name"])
+      fmt.Printf("\tdescription : %v\n", attachment["description"])
       fmt.Printf("\tid : %v\n", attachment["id"])
       fmt.Printf("\tis_obsolete : %v\n", attachment["is_obsolete"])
       fmt.Printf("\tlast_change_time : %v\n", attachment["last_change_time"])
    }
+
+   fmt.Printf("\nid_list : %v\n", id_list)
+
+   // fetch attachments
+   err = client.Call(
+                       "Bug.attachments",
+                       xmlrpc.Struct{
+                          "attachment_ids":id_list,
+                          "include_fields":[]string{
+                             "id","file_name",
+                             "is_obsolete","data"},
+                       },
+                       &result,
+                    )
+   check(err)
+   fmt.Printf("Bug.attachments() returned: %v\n\n", result)
 }
